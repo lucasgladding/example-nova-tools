fc7597e5fb0caf576dd6b60e838aca32b820c5eb
Lucas Gladding <l.gladding@vehikl.com>
2024-01-31 12:33:16 -0500

    Added link to nested page.

--- nova-components/LoremIpsum/resources/js/pages/LoremIpsum.vue
+++ nova-components/LoremIpsum/resources/js/pages/LoremIpsum.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <h1 class="font-normal text-xl mb-4">Lorem Ipsum</h1>
-        <p>{{ text }}</p>
+        <p class="mb-4">{{ text }}</p>
+        <Link href="/nova/lorem-ipsum/dolor-sit" class="text-primary-500">More info</Link>
     </div>
 </template>
 
